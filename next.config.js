05a6d65d4f0566329dbdf2e9ec452f09f967b8b6
PWXJ <fli030119@gmail.com>
2025-06-03 19:33:47 +0800

    初始化项目！！！

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { unoptimized: true },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+};
+
+module.exports = nextConfig;
